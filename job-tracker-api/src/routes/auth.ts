9866f07bc5e03e34bac3fea1ddeef089ec6a8d4b
Michael Kim <michael.yeob@gmail.com>
2026-05-16 05:39:28 -0700

    API for log in & provide JWT

--- job-tracker-api/src/routes/auth.ts
+++ job-tracker-api/src/routes/auth.ts
@@ -1,18 +1,31 @@
 import { Router } from "express"
 import type { Request, Response } from "express";
+import jwt  from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import { pool } from "../db/pool.js"
+import { z } from "zod";
 
 const router = Router();
 
-router.post("/register", async(req: Request, res: Response) => {
-  const { email, password } = req.body;
+const registerSchema = z.object({
+  email: z.email(),
+  password: z.string().min(8),
+});
 
-  if(!email || !password){
-    return res.status(400).json({ error: "Email and password are required"});
+const loginSchema = z.object({
+  email: z.email("Invalid email format"),
+  password: z.string().min(8, "Password must be at least 8 characters")
+})
+
+
+router.post("/register", async (req: Request, res: Response) => {
+  const parsed = registerSchema.safeParse(req.body);
+  if (!parsed.success) {
+    return res.status(400).json({ error: parsed.error.flatten().fieldErrors });
   }
 
-  //hasing password
+  const { email, password } = parsed.data;
+
   const password_hash = await bcrypt.hash(password, 12);
 
   //store in DB
@@ -31,5 +44,48 @@ router.post("/register", async(req: Request, res: Response) => {
     res.status(500).json({ error: "Failed to create user"})
   }
 });
+
+router.post("/login", async (req: Request, res:Response)=> {
+    const parsed = loginSchema.safeParse(req.body);
+
+    if(!parsed.success){
+      return res.status(401).json({ error: parsed.error.flatten().fieldErrors })
+    }
+    const { email, password } = parsed.data
+
+    try {
+      const userResult = await pool.query(
+        "SELECT * FROM users WHERE email = $1", [email]
+      )
+      if(!userResult.rows[0]){
+        return res.status(401).json( { error: "user dosen't exist"});
+      }
+
+      const user = userResult.rows[0];
+      const ok = await bcrypt.compare(password, user.password_hash);
+      if(!ok){
+        return res.status(401).json({ error: "email and password dosen't match"});
+      } 
+
+      const token = jwt.sign(
+        {userID: user.id},
+        process.env.JWT_SECRET!,
+        { expiresIn : "15m"}
+      )
+        return res.status(200).json({
+          token, 
+          user: {
+            id: user.id,
+            email: user.email,
+        }
+      })
+    } catch (error: any){
+      return res.status(401).json( { error: "server error"})
+    }
+  });
+
+
+
+
 
 export default router;
